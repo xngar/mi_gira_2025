43178cd666b2ef42d35f5c8a3111944f0a9420c9
xngar <mantonio.zr@gmail.com>
2025-09-05 18:57:01 -0400

    arreglo 9

--- app/detalle-programa/[id]/page.tsx
+++ app/detalle-programa/[id]/page.tsx
@@ -3,12 +3,13 @@
 import DetallePrograma from "@/components/DetallPrograma";
 import { ProgramaDetalle } from "@/types/types";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: any) {
+  const id = props.params.id;
+
   const response = await fetch(
-    `https://services.migira.cl/api/Migira/Programa/${params.id}`,
+    `https://services.migira.cl/api/Migira/Programa/${id}`,
     { cache: "no-store" }
   );
-
   const data = await response.json();
   const programa: ProgramaDetalle = data.value.entity;
 
