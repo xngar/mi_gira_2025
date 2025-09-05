6f636002711ad95292fd5072573de89b8f8c8f2a
xngar <mantonio.zr@gmail.com>
2025-09-05 18:50:08 -0400

    arreglo 7

--- app/detalle-programa/[id]/page.tsx
+++ app/detalle-programa/[id]/page.tsx
@@ -1,17 +1,17 @@
+// app/detalle-programa/[id]/page.tsx
+
 import DetallePrograma from "@/components/DetallPrograma";
 import { ProgramaDetalle } from "@/types/types";
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string };
-}): Promise<JSX.Element> {
-  const response = await fetch(
-    `https://services.migira.cl/api/Migira/Programa/${params.id}`,
-    { cache: "no-store" } // asegura que no se quede en cache en Vercel
-  );
-  const data = await response.json();
+export default async function Page(props: any) {
+  const id = props.params.id; // 👈 funciona sin pelearse con los tipos
 
+  const response = await fetch(
+    `https://services.migira.cl/api/Migira/Programa/${id}`,
+    { cache: "no-store" }
+  );
+
+  const data = await response.json();
   const programa: ProgramaDetalle = data.value.entity;
 
   return <DetallePrograma programa={programa} />;
